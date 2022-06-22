3db3ba543988c7203ba9b35d84ad6db20a1516c0
Suman Bhandari <93808238+SumanBhandari12@users.noreply.github.com>
2022-06-22 19:46:20 +0545

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,44 +1,74 @@
-# import random
-# numbers =[1,2,3,4,5,6,7,8,9,10]
+ # creating a blue print of an object
+class Student():
+    pass
 
-# length = int(input("Enter the length that you want of your password."))
+# creating a instance of a class
+new_student = Student()
 
-# password = ""
-# for i in range(length-1) :
-#     random_word = str(random.choice(numbers))
+new_student.name = "Suman Bhandari"
+new_student.grade = "XI"
+
+print(new_student)
+# <__main__.Student object at 0x10041fc10>
+# Is the location where my class is located
+
+# Classes are used to model the real world objects like Student or human(common noun)
+class Human():
     
-#     password += random_word;
+    # Creating a constructor that holds the value of the common property that each object have but with different value
+    def __init__(self, gender,age,nationality):
+        
+        # Initiallizing the values
+        
+        self.gender = gender
+        self.age = age 
+        self.nationality = nationality 
+        
+    def __str__(self):
+        print(f"Hello This is me from {self.nationality} ")
+        
+        
+        
+a_human = Human("Male",18,"Nepal")
+print(a_human.__str__())
+
+
+# Conditional inside a class 
+# You can go inside if only you are above 18 and vegetarian
+
+class Person():
     
-# print(password)
+    def __init__(self,name,isVeg,age,gender):
+        self.name = name
+        self.isVeg = isVeg
+        self.age = age
+        self.gender = gender
+    
+    # check the gender of the Person
+    def gender(self):
+        if self.gender == "Male":
+            return "Mr."
+    
+        elif self.gender == "Female":
+            return "Ms."
+    
+        else:
+            return "Mr./Ms."
+        
+    # Function inside a class is known as method  
+    def isAllowed(self):
+        
+        if self.age >18 and self.isVeg == True:
+        # we can only access the method using self keyword in class
+            return f"You are allowed to enter inside {self.name}"
+            
+        else:
+            return f"You are not allowed to enter inside. Hope you understand {self.name}"
+        
+        
+#creating the object with different qualities  
+person1 = Person("Suman Bhandari",False,18,"Male")
+person2 = Person("Bidhana Bhandari",True,19,"Male")
 
-
-# =====================truthy and falsly value===========
-# name = input("Enter your userName")
-# pass_word = input("Enter your password")
-
-
-# if name and pass_word: 
-#     print("true")
-# else:
-#     print("Please enter valid details.")
-
-
-# =========================ternary operator============================
-
-# name = "uman"
-# # condition if true condition if false
-# print(f"Hello {name}" if name == "Suman" else "Hello Stranger")
-
-# helo = "Hello Hello"
-# print(helo or "Nothing")
-
-
-print(True ==1) #true
-print('' == 1) #false as "" is 0
-print([] == 1) #false as [] is empty
-print(10 == 10.0) # false as data types are different
-print([] == []) # true
-
-# first x in the condition of is defining x and second x is looping through the given range
-z= [x for x in range(0,10) if x % 3 ==0]
-print(z)
+# calling the method
+print(person1.isAllowed())
